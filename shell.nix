ca62b71cfdd40a49626ce88a203cc1f97541cb52
zushi <eatingzushidot@proton.me>
2026-03-31 21:08:59 -0400

    refactor use sprinkles

--- shell.nix
+++ shell.nix
@@ -1,8 +1,1 @@
-let
-  sources = import ./npins;
-  pkgs = import sources.nixpkgs { };
-in
-pkgs.mkShell {
-  inputsFrom = [ (pkgs.callPackage ./package.nix { }) ];
-  packages = [ pkgs.clang-tools pkgs.npins ];
-}
+(import ./.).shells.default
