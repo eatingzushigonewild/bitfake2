efe8dfa52a6f37bee681ac05704ca3676154b90e
zushi <eatingzushidot@proton.me>
2026-04-02 15:51:09 -0400

    bump package to 2.0, remove sprinkles, update shell.nix etc (#55)
    * revert
    
    * Update flake.nix
    
    * (/shell.nix): some changes
    
    * (/package.nix): version 2.0
    
    * chore: update flake.lock and npins
    
    * /npins/sources.json: remove sprinkles
    
    not needed
    
    ---------
    
    Co-authored-by: eatingzushigonewild <271111067+eatingzushigonewild@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -1,1 +1,8 @@
-(import ./.).shells.default
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs { };
+in
+pkgs.mkShell {
+  inputsFrom = [ (pkgs.callPackage ./package.nix { }) ];
+  packages = [ pkgs.clang-tools pkgs.npins ];
+}
